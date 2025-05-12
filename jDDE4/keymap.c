b5715e840eab398dee36c85852739e1e7551bc29
Ryan Hughes <1630358+ryanrhughes@users.noreply.github.com>
2025-05-11 22:23:50 -0400

    Update keymap.c

--- jDDE4/keymap.c
+++ jDDE4/keymap.c
@@ -414,5 +414,4 @@ const char chordal_hold_layout[MATRIX_ROWS][MATRIX_COLS] PROGMEM =
   'L', 'L', 'L', 'L', 'L', 'L',             'R', 'R', 'R', 'R', 'R', 'R',
   'L', 'L', 'L', 'L', 'L', 'L',             'R', 'R', 'R', 'R', 'R', 'R',
                            '*', '*',   '*', '*'
-  )
-);
+  );
